be3e804e42cb2f5e18fb8c0552e59c217e6127ef
Sharmila6111 <sharmilas2207@gmail.com>
2024-11-05 14:20:32 +0530

    https://github.com/Sharmila6111/sharmila-Portfolio.git

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>SHARMILA S</h1>
+        <h3>WEB DEVELOPER</h3>
+        <hr></hr>  
+
+        <h5>1/108 Mela street</h5>
+        <h5>sharmilas2207@gmail.com</h5>
+        <h5>8531945351 </h5>
+        <hr></hr>
+
+        <h3>PROFILE</h3>
+        <p>
+         I am qualified amd professonal web developer strong creative and analytical skills. team player with an eye for details.seeking for an entry level opportuniies with an eseteemed organisation where i can utilise nmy skills and enhance learning in the field of work.
+        </p>
+        <hr></hr>
+         
+            <h3>TECHNICAL SKILLS</h3>
+        <ul>
+            <li> html</li>
+            <li>css</li>
+            <li> github</li>
+            <li> python</li>
+        </ul>
+        <hr></hr>
+        <h3> EDUCATION</h3>
+        <h4> Bachelor of Engineering</h4>
+        <h4> Vetri Vinayaha College of Engineering & Technology</h4>
+        <hr></hr>
+        <h3> SKILLS</h3>
+        <u1> 
+            <li> Leadership</li>
+            <li> Discipline</li>
+            <li> Self-confident</li>
+        </u1>
+        <hr></hr>
+        <h3>PROJECT</h3>
+        <h4>portfolio</h4>
+        <p>
+            The portfolio id developed using reactjs a popular javascript library for building user interface.Additionally technolgy include Html,css.
+            <hr></hr>
+            <h3>CERTIFICATION</h3>
+            <h4> Full stack development</h4>
+            <h5> Novitech Coimbatore</h5>
+        </p>
+    </div>
+  );
+}
+
+export default App;
